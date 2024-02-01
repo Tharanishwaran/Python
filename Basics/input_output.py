2796fdd5fc6e0b7c8874099a5ca88dc43601bcd3
Tharanishwaran <133676934+Tharanishwaran@users.noreply.github.com>
2024-02-01 22:07:24 +0530

    input output added

---
+++ Basics/input_output.py
@@ -0,0 +1,17 @@
+ 
+# getting input from user 
+name = input("Enter your name: ") 
+
+
+print("hello", name)
+
+
+# accepting integer from the user 
+# the return type of input() function is string , 
+# so we need to convert the input to integer 
+number1 = int(input("Enter num1: ")) 
+number2 = int(input("Enter num2: ")) 
+
+number3 = number1 * number2 
+print("Product is: ", number3)
+
