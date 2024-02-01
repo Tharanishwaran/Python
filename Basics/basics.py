0b698660931685e26d93c1ae34eb4ec80c05e599
Tharanishwaran <133676934+Tharanishwaran@users.noreply.github.com>
2024-02-01 22:01:28 +0530

    basics upladed

---
+++ Basics/basics.py
@@ -0,0 +1,18 @@
+#single line command
+"""this is multi line command"""
+
+
+
+var_int = 21 #no need to define the data type
+
+var_float = 21.6
+
+var_char = 'a'
+
+print(var_int)
+
+print(var_float)
+
+print(var_char)
+
+print("hello python")   #string
