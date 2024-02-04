1a541b2c27a79b81d36627901ead0a1a4c027060
Tharanishwaran <133676934+Tharanishwaran@users.noreply.github.com>
2024-02-04 22:29:01 +0530

    operators in python added

---
+++ Basics/operators.py
@@ -0,0 +1,22 @@
+# and, or, not, is and in keyword implementation in Python
+
+print(True or False)
+
+print(False and True)
+
+print(not True)
+
+if 's' in 'geeksforgeeks':
+    print("s is part of geeksforgeeks")
+
+else:
+    print("s is not part of geeksforgeeks")
+
+for i in 'geeksforgeeks':
+    print(i, end=" ")
+
+print("\r")
+
+print(' ' is ' ')
+
+print({} is {})
